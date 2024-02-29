f09da682afa63f98b0a08407a968498a5693cfb3
SNEHA D <124783299+Sneha1708@users.noreply.github.com>
2024-02-29 09:21:42 +0530

    Add files via upload
    CC2

---
+++ backend/Project/Project/src/Project/Project/src/main/java/com/example/Project/service/SignUpService.java
@@ -0,0 +1,55 @@
+package com.example.Project.service;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+
+import org.springframework.stereotype.Service;
+
+import com.example.Project.model.SignUp;
+import com.example.Project.repository.SignUpRepo;
+
+@Service
+public class SignUpService {
+    @Autowired
+    SignUpRepo repo;
+
+    public SignUp create(SignUp j) {
+        return repo.save(j);
+        // return true;
+    }
+
+    public SignUp find(int id) {
+        return repo.findById(id).orElse(null);
+    }
+
+    public List<SignUp> findA() {
+        return repo.findAll();
+    }
+
+    public SignUp update(int id, SignUp j) {
+        SignUp p1 = repo.findById(id).orElse(null);
+        if (p1 != null) {
+            p1.setName(j.getName());
+            repo.save(p1);
+            return p1;
+        } else {
+            return j;
+        }
+    }
+
+    public void delete(int id) {
+        repo.deleteById(id);
+
+    }
+
+    public Page<SignUp> findAllPaginatedAndSorted(int pageNo, int pageSize, String sortBy, String direction) {
+        Sort sort = Sort.by(Sort.Direction.fromString(direction), sortBy);
+        Pageable pageable = PageRequest.of(pageNo, pageSize, sort);
+        return repo.findAll(pageable);
+    }
+}
